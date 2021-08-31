28cb069d2325332cccbffef88ee0cd4813658237
rishika kumari <78673968+rishika789@users.noreply.github.com>
2021-08-31 14:49:57 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -40,10 +40,7 @@ export const Contact = (props) => {
             <div className='row'>
               <div className='section-title'>
                 <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
+                
               </div>
               <form name='sentMessage' validate onSubmit={handleSubmit}>
                 <div className='row'>
